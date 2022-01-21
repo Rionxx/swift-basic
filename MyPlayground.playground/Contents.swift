b0487939aea7c5e3825005ebc306ead9d1be6464
Rion <rionfx.42@gmail.com>
2022-01-21 19:00:49 +0900

    Complete Part2

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+//出力
+/*
+print("Hello World")
+print(1+1)
+print("ヤァ" + "のび太")
+*/
+ 
+//変数
+/*
+var num = 3
+print(num + 2)
+
+var name = "のび太"
+name = "Rion"
+print(name)
+
+let name2 = "のび太"
+print(name2)
+*/
